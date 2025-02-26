bdb8186293aafc4a67585dfb15d7b0ea3fb7c06c
ishaq60 <ishaqahammad60@gmail.com>
2025-02-27 00:18:14 +0600

    Use tankstack query

--- clientSide/src/main.jsx
+++ clientSide/src/main.jsx
@@ -6,12 +6,18 @@ import router from "./Routes/Routes";
 import AuthProvider from "./Provider/AuthProvider";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client instance
+// const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster></Toaster>
-    </AuthProvider>
+  z
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </AuthProvider>
+
   </StrictMode>
 );
